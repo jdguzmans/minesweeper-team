488496895e15d3f92ee0a2c720c6f69f45051ab7
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-10-04 12:27:58 -0500

    added chatbox capability

--- client/main.js
+++ client/main.js
@@ -3,7 +3,14 @@ import { Meteor } from 'meteor/meteor'
 import { render } from 'react-dom'
 
 import App from '../imports/ui/App.jsx'
+import { Chat } from '/imports/api/chat';
+
+var localVariable;
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'))
+ 
+  localVariable = Chat;
+  console.log(Chat);
+
 })

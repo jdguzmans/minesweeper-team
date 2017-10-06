d6b1ffa402a6fd5cdebbcff1671f2f2251ee2988
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2017-10-06 14:44:46 -0500

    indicator styling

--- imports/ui/Square.js
+++ imports/ui/Square.js
@@ -4,10 +4,13 @@ class Square extends Component {
   render () {
     return (
       <button
-        className='square'
+        //className='square'
+        className= {this.props.square.isSelected ? 'Cell__number' + this.props.square.value : 'square'}
         disabled={this.props.isSelected}
         onClick={this.props.selectSquare}>
+
         {this.props.square.isSelected ? this.props.square.value : '?'}
+        
       </button>
     )
   }

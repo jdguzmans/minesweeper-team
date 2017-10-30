26559ab178f2cb5489a21fdccb9697ec907a539e
jdguzmans <jdguzmans@hotmail.com>
2017-10-30 12:54:27 -0500

    commit para pode rahcer deploy

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor'
 import { Inject } from "meteor/meteorhacks:inject-initial"
-import '../imports/api/games.js'
+import '../imports/api/games.js' 
 import '../imports/api/scores.js'
 
 Meteor.startup(() => {

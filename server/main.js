607e9e7eea3bc1f9ec4895856599a61f695fa560
carlox18 <ce.gonzalez13@uniandes.edu.co>
2017-10-31 03:53:59 -0500

    Bien por la accesibilidad

--- server/main.js
+++ server/main.js
@@ -9,3 +9,5 @@ Meteor.startup(() => {
     return html.replace(/<html>/, '<!-- HTML 5 -->\n<html lang="en">')
   })
 })
+// Bien que hayan hecho esto del lado del servidor para que apenas cargue la página agregue el tag html con el lenguaje, así las herramientas
+// de accesibilidad como las usadas por los ciegos pueden identificar el lenguaje y hacer mejor su trabajo.
